a806397d2ceb4a417c14e6d8bdc492b5c6f3e6a7
Oujir002 <Oujir002@github.com>
2020-02-18 13:42:17 -0600

    Added window impl
    Mouse clicks are handled as they are planned to be.  Conditions are subject to change when image processing becomes involved.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # main.py
 
-# code goes here
+import tkinter as tk 
+
+# Window class to handle all inputs and represent visuals
+class Window(tk.Frame): 
+    # initialization of window
+    def __init__(self, master):
+        self.master = master
+        tk.Frame.__init__(self, master)
+        # configures frame to size of 100,100
+        self.config(width=100, height=100)
+        # binds and sets location that will be handled
+        self.bind("<Button-1>", self.handle_mouse_click) 
+        self.pack()
+ 
+    # a debugging function to show where user clicked
+    def handle_mouse_click(self, event): 
+        position = "(%s, %s)" % (event.x, event.y)
+        print("clicked @: " + position)
+
+# creates the window
+app = Window(master=tk.Tk())
+app.mainloop()
