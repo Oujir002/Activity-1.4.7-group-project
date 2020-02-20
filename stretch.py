7c3bfb7aca2c8d434bb9d9cd4838e807810311f8
aloren004 <59578363+aloren004@users.noreply.github.com>
2020-02-20 12:29:16 -0600

    Update stretch.py
    Rewrote stretch so the stretch is decremental.

--- stretch.py
+++ stretch.py
@@ -13,42 +13,30 @@ new_img = PIL.Image.open(original_image)
 original_img = PIL.Image.open(original_image)
 fig, axes = plt.subplots(1, 2)
 axes[0].imshow(original_img, interpolation='none')
-
-power = 3
-
-pos = (200,200)
-br_pos = (220,220)
-#top right
-x_mid = (pos[0]+br_pos[0])/2
-y_mid = (pos[1]+br_pos[1])/2
-for y in range(pos[1]-1,y_mid+1):
-    for x in range(x_mid-1,br_pos[0]+1):
-        for ix in range(power):
-            for iy in range(power):
-                new_img.putpixel((x+(x-x_mid)*(power-1)+ix,y-(y_mid-y)*(power-1)-iy),original_img.getpixel((x,y)))
-#bottom right
-for y in range(y_mid-1,(br_pos[1])+1):
-    for x in range(x_mid-1,br_pos[0]+1):
-        for ix in range(power):
-            for iy in range(power):
-                new_img.putpixel((x+(x-x_mid)*(power-1)+ix,y+(y-y_mid)*(power-1)+iy),original_img.getpixel((x,y)))
-#bottom left
-for y in range(y_mid-1,(br_pos[1])+1):
-    for x in range(pos[0]-1,x_mid+1):
-        for ix in range(power):
-            for iy in range(power):
-                new_img.putpixel((x-(x_mid-x)*(power-1)-ix,y+(y-y_mid)*(power-1)+iy),original_img.getpixel((x,y)))
-#top left
-for y in range(pos[1]-1,y_mid+1):
-    for x in range(pos[0]-1,x_mid+1):
-        for ix in range(power):
-            for iy in range(power):
-                new_img.putpixel((x-(x_mid-x)*(power-1)-ix,y-(y_mid-y)*(power-1)-iy),original_img.getpixel((x,y)))
-
-
+power = 15
+x_iterator = 0 #decreases the number of pixels for the next section
+y_iterator = 0
+x_distance_from_center = 0
+y_distance_from_center = 0 #how far away the drawn pixel is from the center
+x = 80
+y = 60
+for y in range(y,y+power): #repeats for the power
+    for i in range(power-y_iterator): #decrements for every pixel in the y axis
+        for x in range(x,x+power): #repeats for the power
+            for j in range(power-x_iterator): #decrements for every pixel in the x axis
+                new_img.putpixel((x+x_distance_from_center,y+y_distance_from_center),original_img.getpixel((x,y)))
+                x_distance_from_center +=1 #moves the drawn pixel one to the right 
+            x_distance_from_center -=1 #removes the gaps
+            x_iterator +=1 #decrements in the x direction
+        y_distance_from_center +=1 #moves the drawn pixel one down
+        x_iterator = 0 # resets the x iterator
+        x_distance_from_center = 0 #resets the x distance from center
+        x=80 #resets the x pos
+    y_distance_from_center -=1 #removes the gap
+    y_iterator +=1 #decrements in the y direction
+y = 60 #resets the y pos
+    
 
 # Display student in second axes and set window to the right eye
 axes[1].imshow(new_img, interpolation='none')
 fig.show()
-
-
